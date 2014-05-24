9125a5d33cba69097b580b7aaac9b823432cb19b
sailorphoenix <anais.hervouin@gmail.com>
2014-05-24 19:06:41 +0200

    Init jquery validate sur le formulaire de contact

---
+++ web/js/documentready.js
@@ -0,0 +1,38 @@
+function contactFormValidate(){
+    $('#footer-contact-form').validate({
+        onsubmit: true,
+        onkeyup: true,
+        rules : {
+            'contact[nom]' : {
+            	required: true
+            },
+            'contact[prenom]' : {
+            	required: true
+            },
+            'contact[mail]' : {
+            	email: true,
+            	required : true
+            },
+            'contact[objet]' : {
+            	maxlength: 10,
+            	required: true
+            },
+            'contact[message]' : {
+            	required: true
+            }
+        }
+    });
+}
+
+function heightMenuRWD (){
+	var height = $('#main-content').height();
+	$('#trigger').height(height + 1);
+}
+
+$(document).ready(function (){
+    heightMenuRWD();  
+});
+
+$(window).resize(function () {
+	heightMenuRWD();
+});
