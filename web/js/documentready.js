1af24efa153060f7178ce017031fc13dff5c4ad1
sailorphoenix <anais.hervouin@gmail.com>
2014-05-26 21:24:48 +0200

    Ajout d'un smooth scrolling dans le cas où le captcha est erroné

--- web/js/documentready.js
+++ web/js/documentready.js
@@ -1,39 +1,22 @@
-function contactFormValidate(){
-    $('#footer-contact-form').validate({
-        onsubmit: false,
-        onkeyup: true,
-        rules : {
-            'contact[nom]' : {
-            	required: true
-            },
-            'contact[prenom]' : {
-            	required: true
-            },
-            'contact[mail]' : {
-            	email: true,
-            	required : true
-            },
-            'contact[objet]' : {
-            	maxlength: 10,
-            	required: true
-            },
-            'contact[message]' : {
-            	required: true
-            }
-        }
-    });
+function heightMenuRWD (){
+    var height = $('#main-content').height();
+    $('#trigger').height(height + 1);
 }
 
-function heightMenuRWD (){
-	var height = $('#main-content').height();
-	$('#trigger').height(height + 1);
+function errorCaptcha (){
+    var next = $('label[for=contact_captcha]').next();
+    var li = next.children('li');
+    if (li.length) {
+        smoothScroll.animateScroll( null, '#formulaire-contact' );
+    }
 }
 
 $(document).ready(function (){
-    heightMenuRWD(); 
-    contactFormValidate();
+    heightMenuRWD();
+    smoothScroll.init();
+    errorCaptcha();
 });
 
 $(window).resize(function () {
-	heightMenuRWD();
+    heightMenuRWD();
 });
